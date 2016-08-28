8a9f81678e50dc172b0039562a002070c32d6c5a
gregorybesson <gregory.besson@adfab.fr>
2016-08-28 07:35:02 +0200

    time spent on select more precise

--- routes/post/data.js
+++ routes/post/data.js
@@ -5,7 +5,6 @@ var route = function route(req, res, next, abe) {
   if(typeof res._header !== 'undefined' && res._header !== null) return;
 
   var htmlToSend = '';
-  var dateStart = new Date()
   var request = '{{abe type="data" key="results" source="' + req.body.select + '"}}';
   var json = req.body.json
   if(typeof json !== 'undefined' && json !== null
@@ -15,17 +14,17 @@ var route = function route(req, res, next, abe) {
     json = {}
   }
 
+  var dateStart = new Date()
   var result = abe.Sql.getDataRequest(
     req.body.path,
     request,
     json
   )
+  var d = (new Date().getTime() - dateStart.getTime()) / 1000
 
   var login = abe.fileUtils.concatPath(__dirname + '/../../partials/data.html')
   var html = abe.fileUtils.getFileContent(login);
-
   var template = abe.Handlebars.compile(html, {noEscape: true})
-  var d = (new Date().getTime() - dateStart.getTime()) / 1000
   var tmp = template({
     express: {
       req: req,
