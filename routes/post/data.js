0c7a1ea56766d689c42c209c3f1472642c120c69
gregorybesson <gregory.besson@adfab.fr>
2016-08-29 13:17:00 +0200

    new version

--- routes/post/data.js
+++ routes/post/data.js
@@ -15,31 +15,29 @@ var route = function route(req, res, next, abe) {
   }
 
   var dateStart = new Date()
-  var result = abe.Sql.getDataRequest(
-    req.body.path,
-    request,
-    json
-  )
-  var d = (new Date().getTime() - dateStart.getTime()) / 1000
+  abe.Sql.executeQuery(req.body.path, request, json)
+    .then((result) => {
+      var d = (new Date().getTime() - dateStart.getTime()) / 1000
 
-  var login = abe.fileUtils.concatPath(__dirname + '/../../partials/data.html')
-  var html = abe.fileUtils.getFileContent(login);
-  var template = abe.Handlebars.compile(html, {noEscape: true})
-  var tmp = template({
-    express: {
-      req: req,
-      res: res
-    },
-    select: req.body.select,
-    path: req.body.path,
-    json: req.body.json,
-    result: result,
-    size: result.length,
-    request: request,
-    duration: d
-  })
-  
-  return res.send(tmp);
+      var login = abe.fileUtils.concatPath(__dirname + '/../../partials/data.html')
+      var html = abe.fileUtils.getFileContent(login);
+      var template = abe.Handlebars.compile(html, {noEscape: true})
+      var tmp = template({
+        express: {
+          req: req,
+          res: res
+        },
+        select: req.body.select,
+        path: req.body.path,
+        json: req.body.json,
+        result: result,
+        size: result.length,
+        request: request,
+        duration: d
+      })
+      
+      return res.send(tmp);
+    })
 }
 
 exports.default = route
